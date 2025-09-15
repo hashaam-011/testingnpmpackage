caeb5aa3f3818fbcbc2e3d2e1f10cfeb06f79a3a
MeerHashaam Khan <mkhan@mobizinc.com>
2025-09-15 14:12:40 +0500

    webpack compilation

---
+++ dist/types/index.d.ts
@@ -0,0 +1,1 @@
+// Type declarations here 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const WebpackObfuscator = require('webpack-obfuscator');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'index.obf.js',
     library: { type: 'commonjs2' }
   },
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new WebpackObfuscator(require('./obfuscator.config.json'), [])
+  ]
 };
